0acab6e759ae2655caebb78c3cb4e11379df0438
masac <msktjm98@gmail.com>
2023-02-03 16:46:34 +0900

    context作成

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {auth} from '../firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 const SignUp = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('登録')
+    // console.log('登録')
+    const { email, password } = e.target.elements;
+    createUserWithEmailAndPassword(auth, email.value, password.value);
   }
 
   return (
---
+++ src/context/AuthContext.js
@@ -0,0 +1,30 @@
+import { createContext, useState, useContext, useEffect } from 'react';
+import { auth } from '../firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState('');
+
+  const value = {
+    user,
+  };
+
+  //サインイン、アウトの監視
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      setUser(user);
+    })
+    return () => {
+      unsubscribed();
+    };
+  }, []);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
