115ce6e6d448c19bfd26ad0f931936d5a93437c6
masac <msktjm98@gmail.com>
2023-02-04 01:10:57 +0900

    homeページの作成まで

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import SignUp from './components/SignUp'
+import Home from './components/Home';
+import Login from './components/Login';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -9,7 +11,9 @@ function App() {
       <div style={{ margin: '2em' }}>
       <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const handleSubmit = () => {
+
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='email'>メールアドレス</label>
+          <input
+            id="email" 
+            name="email" 
+            type="email" 
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>パスワード</label>
+          <input 
+            id="password"
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <div>
+          <button>ログイン</button>
+        </div>
+        <div>
+          ユーザ登録<Link to={'/signup'}>はこちら</Link>から
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
