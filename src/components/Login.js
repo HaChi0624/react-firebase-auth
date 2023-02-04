e189c872e444402dc81621d8c25c33b73ba8568d
masac <msktjm98@gmail.com>
2023-02-04 13:09:37 +0900

    ログイン後のリダイレクト設定

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,42 @@
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
+  //ログアウトを行い、ログイン画面からログインを行ってください。正しいメールアドレスとパスワードを入力してログインを行った場合に/(ルート)にリダイレクトされますが何も入れない場合や間違ったメールアドレスまたはパスワードを入れた場合はブラウザ上には何も起こりません。
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    signInWithEmailAndPassword(auth, email.value, password.value);
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then(() => {
+        navigate('/')
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            setError('正しいメールアドレスの形式で入力してください。');
+            break;
+          case 'auth/user-not-found':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+          case 'auth/wrong-password':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+          default:
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+        }
+      });
   };
 
   return (
     <div>
       <h1>ログイン</h1>
       <form onSubmit={handleSubmit}>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <div>
           <label htmlFor='email'>メールアドレス</label>
           <input
